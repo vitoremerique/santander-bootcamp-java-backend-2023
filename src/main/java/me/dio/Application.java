d9f1ea11be15225bfb6497b6785fd2d9200f9746
vitoremerique <vitort.emerique@hotmail.com>
2023-10-10 11:18:22 -0300

    fix Deploy

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -2,16 +2,22 @@ package me.dio;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
-@OpenAPIDefinition (servers = {@Server(url = "/v1", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public OpenApiCustomizer serverOpenApiCustomiser1() {
+		return openAPI -> openAPI.getServers().forEach(server -> server.setDescription("my new description"));
 	}
 
 }
